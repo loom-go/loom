6b7dc0de7fa179e7bf49fc90c80194920c226793
AnatoleLucet <anatole.lucet@proton.me>
2026-03-05 10:37:49 +0100

    feat(term): scrollbox & inputs

--- components/ref.go
+++ components/ref.go
@@ -7,12 +7,12 @@ import (
 )
 
 // Ref creates a Node that calls the provided function with the parent node as an argument.
-func Ref[T any](fn func(T)) loom.Node {
-	return &refNode[T]{fn}
+func Ref[T any](ref *T) loom.Node {
+	return &refNode[T]{ref}
 }
 
 type refNode[T any] struct {
-	fn func(T)
+	ref *T
 }
 
 func (n *refNode[T]) ID() string {
@@ -24,10 +24,10 @@ func (n *refNode[T]) Mount(slot *loom.Slot) error {
 
 	casted, ok := parent.(T)
 	if !ok {
-		return fmt.Errorf("Ref: %w: the given ref type did not match the node type", ErrNodeRefMissMatch)
+		return fmt.Errorf("Ref: %w: the given ref type (%T) does not match the parent node type (%T)", ErrNodeRefMissMatch, *new(T), parent)
 	}
 
-	n.fn(casted)
+	*n.ref = casted
 	return nil
 }
 
@@ -36,5 +36,38 @@ func (n *refNode[T]) Update(slot *loom.Slot) error {
 }
 
 func (n *refNode[T]) Unmount(slot *loom.Slot) error {
+	return nil
+}
+
+// OnRef creates a Node that calls the provided function with the parent node as an argument.
+func OnRef[T any](fn func(T)) loom.Node {
+	return &onRefNode[T]{fn}
+}
+
+type onRefNode[T any] struct {
+	fn func(T)
+}
+
+func (n *onRefNode[T]) ID() string {
+	return "loom.OnRef"
+}
+
+func (n *onRefNode[T]) Mount(slot *loom.Slot) error {
+	parent := slot.Parent()
+
+	casted, ok := parent.(T)
+	if !ok {
+		return fmt.Errorf("OnRef: %w: the given ref type (%T) does not match the parent node type (%T)", ErrNodeRefMissMatch, *new(T), parent)
+	}
+
+	n.fn(casted)
+	return nil
+}
+
+func (n *onRefNode[T]) Update(slot *loom.Slot) error {
+	return nil
+}
+
+func (n *onRefNode[T]) Unmount(slot *loom.Slot) error {
 	return nil
 }
